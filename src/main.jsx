61582a573fe3b823afac843672bc6e48c6b2ccf6
noyon1122 <noyonhossain910@gmail.com>
2025-01-19 19:25:14 +0600

    product list product card filters completed

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,12 @@ import App from './App.jsx'
 import 'react-multi-carousel/lib/styles.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './utils/routes.jsx';
-import Navbar from './components/navbar/Navbar.jsx';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
    <RouterProvider router={router}>
-    <Navbar></Navbar>
+    
    <App />
    </RouterProvider>
   </StrictMode>,
--- src/pages/ProductListPage/ProductListPage.jsx
+++ src/pages/ProductListPage/ProductListPage.jsx
@@ -1,29 +1,43 @@
 import React, { useEffect, useMemo, useState } from 'react'
 import FilterIcon from '../../components/common/FilterIcon';
 import content from '../../data/content.json';
-import Navbar from '../../components/navbar/Navbar';
 import Categories from '../../components/Filters/Categories';
+import PriceFilter from '../../components/Filters/PriceFilter';
+import ColorsFilter from '../../components/Filters/ColorsFilter';
+import SizeFilter from '../../components/Filters/SizeFilter';
+import ProductCard from './ProductCard';
+
+import { setLoading } from '../../store/features/common';
 
 const categories =content?.categories;
 
 
 const ProductListPage = ({categoryType}) => {
 
- const categoryContent=useMemo(()=>{
-  return categories?.find((category)=>category.code===categoryType);
 
- },[categoryType]) ;
+  const [products,setProducts] = useState([]);
+
+  const categoryContent = useMemo(()=>{
+    return categories?.find((category)=> category.code === categoryType);
+  },[categoryType]);
   
+  const productListItems = useMemo(()=>{
+    return content?.products?.filter((product)=> product?.category_id === categoryContent?.id );
+  },[categoryContent]);
+
+
+
+
 
   return (
     <div>
-    
         <div className='flex'>
             <div className='w-[20%] p-[10px] border rounded-lg m-[20px]'>
                 {/* Filters */}
                 <div className='flex justify-between '>
                 <p className='text-[16px] text-gray-600'>Filter</p>
                 <FilterIcon />
+                
                 </div>
                 <div>
                   {/* Product types */}
@@ -31,7 +45,26 @@ const ProductListPage = ({categoryType}) => {
                 <Categories types={categoryContent?.types}/>
                 <hr></hr>
                 </div>
-               
+                  {/* Price */}
+                  <PriceFilter />
+                  <hr></hr>
+                  {/* Colors */}
+                  <ColorsFilter colors={categoryContent?.meta_data?.colors}/>
+                  <hr></hr>
+                   {/* Sizes */}
+                   <SizeFilter sizes={categoryContent?.meta_data?.sizes}/>
+            </div>
+
+            <div className='p-[15px]'>
+            <p className='text-black text-lg'>{categoryContent?.description}</p>
+                {/* Products */}
+                <div className='pt-4 grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-8 px-2'>
+                {productListItems?.map((item,index)=>(
+                  <ProductCard key={item?.id+"_"+index} {...item}  title={item?.title}/>
+                ))}
+                 
+          
+                </div>
 
             </div>
 
